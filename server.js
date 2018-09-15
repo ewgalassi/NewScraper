e20d3fb7e8ac3c30e5d3f6d2ad4e1905d975a1ee
Muscle Everest <danza_seppia@yahoo.com>
2018-09-15 14:58:55 -0500

    initial setup

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+var mongoose = require("mongoose");
+var express = require("express");
+var bodyParser = require("body-parser");
+var request = require("request");
+var app = express();
+
+var PORT = process.env.PORT || 7000;
+
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
@@ -5,3 +13,26 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+    console.log("Connected")
+});
+
+var articleSchema = mongoose.Schema;
+
+request("https://newsapi.org/v2/top-headlines?" +
+    "country=us&" +
+    "apiKey=585650be8ce14a0b99a8026c4ff538f2", function (error, response, body) {
+        console.log("error:", error);
+        console.log("statusCode:", response && response.statusCode);
+        console.log("body:", body);
+    });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, function () {
+    console.log("Listening on port " + PORT)
+});
